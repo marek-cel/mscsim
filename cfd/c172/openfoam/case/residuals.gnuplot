ae5fb8e39d4ba5c8fb5d896143475376d12852a4
marek-cel <46421808+marek-cel@users.noreply.github.com>
2019-05-26 15:21:41 +0200

    Cessna 172 OpenFOAM simulations case and run script

--- cfd/c172/openfoam/case/residuals.gnuplot
+++ cfd/c172/openfoam/case/residuals.gnuplot
@@ -3,4 +3,6 @@ plot \
   "logs/p_0" with lines, \
   "logs/Ux_0" with lines, \
   "logs/Uy_0" with lines, \
-  "logs/Uz_0" with lines
+  "logs/Uz_0" with lines, \
+  "logs/k_0" with lines, \
+  "logs/omega_0" with lines
--- cfd/c172/openfoam/case/run.sh
+++ cfd/c172/openfoam/case/run.sh
@@ -31,27 +31,31 @@ decomposePar -copyZero > log.decomposePar 2>&1
 echo "Running snappyHexMesh in parallel using $(getNumberOfProcessors) processes"
 mpirun -np $(getNumberOfProcessors) snappyHexMesh -parallel -overwrite > log.snappyHexMesh 2>&1
 
+echo "Running renumberMesh in parallel using $(getNumberOfProcessors) processes"
+mpirun -np $(getNumberOfProcessors) renumberMesh -parallel -overwrite > log.renumberMesh 2>&1
+
 # Mesh preview
 # echo "Running reconstructParMesh"
 # reconstructParMesh -constant
+# exit 1
 
 ################################################################################
 
-# echo "Running patchSummary in parallel using $(getNumberOfProcessors) processes"
-# mpirun -np $(getNumberOfProcessors) patchSummary -parallel > log.patchSummary 2>&1
+echo "Running patchSummary in parallel using $(getNumberOfProcessors) processes"
+mpirun -np $(getNumberOfProcessors) patchSummary -parallel > log.patchSummary 2>&1
 
-# echo "Running potentialFoam in parallel using $(getNumberOfProcessors) processes"
-# mpirun -np $(getNumberOfProcessors) potentialFoam -parallel > log.potentialFoam 2>&1
+echo "Running potentialFoam in parallel using $(getNumberOfProcessors) processes"
+mpirun -np $(getNumberOfProcessors) potentialFoam -parallel > log.potentialFoam 2>&1
 
 echo "Running $(getApplication) in parallel using $(getNumberOfProcessors) processes"
 mpirun -np $(getNumberOfProcessors) $(getApplication) -parallel > log.$(getApplication) 2>&1
+# pyFoamPlotWatcher.py log.$(getApplication)
 
 echo "Running reconstructParMesh"
 reconstructParMesh -constant > log.reconstructParMesh 2>&1
 
 echo "Running reconstructPar"
 reconstructPar -latestTime > log.reconstructPar 2>&1
-# reconstructPar > log.reconstructPar 2>&1
 
 echo "Running foamLog"
 foamLog log.$(getApplication)
