34d577d58fb149e083491d62b0ad55b6b2fc780c
marek-cel <46421808+marek-cel@users.noreply.github.com>
2020-06-20 22:17:12 +0200

    dpkg files

--- deb_create.sh
+++ deb_create.sh
@@ -7,7 +7,7 @@ APP_VER=0.3
 
 ################################################################################
 
-APP_DIR=${APP_NAME}_${APP_VER}_focal_amd64
+APP_DIR=${APP_NAME}_${APP_VER}_bionic_amd64
 
 sudo rm -R $APP_DIR
 sudo rm $APP_DIR.deb
@@ -34,26 +34,6 @@ sudo chmod 0755 $APP_DIR/usr/share/applications/mscsim.desktop
 sudo chmod 0755 $APP_DIR/usr/share/icons/hicolor/128x128/apps/mscsim.png
 
 dpkg -b $APP_DIR/
-
-################################################################################
-
-DATA_DIR=${APP_NAME}-data_${APP_VER}_all
-
-sudo rm -R $DATA_DIR
-sudo rm $DATA_DIR.deb
-
-mkdir $DATA_DIR
-mkdir $DATA_DIR/DEBIAN
-mkdir $DATA_DIR/usr
-mkdir $DATA_DIR/usr/share
-mkdir $DATA_DIR/usr/share/mscsim
-
-cp deb_mscsim-data_all.control $DATA_DIR/DEBIAN/control
-cp -R data/ $DATA_DIR/usr/share/mscsim
-
-sudo chown root:root -R $DATA_DIR/
-
-dpkg -b $DATA_DIR/
 
 ################################################################################
 
