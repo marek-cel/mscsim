842c7c9a4b132a9f8db9b4ccbd1325c84df4e291
marek-cel <46421808+marek-cel@users.noreply.github.com>
2021-01-23 12:08:55 +0100

    dpkg scripts and input files

--- deb_clean.sh
+++ deb_clean.sh
@@ -13,11 +13,4 @@ sudo rm $APP_DIR.deb
 
 ################################################################################
 
-DATA_DIR=${APP_NAME}-data_*_focal_all
-
-sudo rm -R $DATA_DIR
-sudo rm $DATA_DIR.deb
-
-################################################################################
-
 read -p "Press any key to continue..." -n1 -s
