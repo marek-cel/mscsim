ec8be59511dcc51eba91060754274e6ff1c87e95
Marek Cel <marek.cel@gmail.com>
2021-12-17 19:47:41 +0100

    Cleanup.

--- sync_github.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rsync -avu --delete --exclude '*.dropbox*' ./src/ ~/dev/git/mscsim/trunk/src/
-
-notify-send "Syncing finished."
-read -p "Press any key to continue... " -n1 -s
--- sync_github_cc0.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rsync -avu --delete --exclude '*.dropbox*' ./src_cc0/ ~/dev/git/mscsim-cc0/trunk/src/
-
-notify-send "Syncing finished."
-read -p "Press any key to continue... " -n1 -s
